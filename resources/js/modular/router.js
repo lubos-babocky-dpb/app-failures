468641e2c732b9e438202ea5e7a9f9959a831fd3
Lubos Babocky <Babocky.Lubos@dpb.sk>
2026-09-01 13:34:34 +0200

    Replace old report form with new CreateFailureReport.vue page

--- resources/js/modular/router.js
+++ resources/js/modular/router.js
@@ -1,15 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import UserInfoPage from "./Pages/UserInfoPage.vue";
 import DebugPage from "./Pages/DebugPage.vue";
-import ReportPage from "./Pages/ReportPage.vue";
 import ReportableAssetsPage from "./Pages/ReportableAssetsPage.vue";
+import CreateFailureReport from "./Pages/CreateFailureReport.vue";
 
 const router = createRouter({
     history: createWebHistory('/'),
     routes: [
         {
             path: '/',
-            component: () => import('./Pages/NewReport.vue'),
+            component: CreateFailureReport,
             meta: {
                 labelKey: 'nav.new_report',
                 icon: 'M12 4v16m8-8H4',
@@ -39,15 +39,6 @@ const router = createRouter({
             component: ReportableAssetsPage,
             meta: {
                 labelKey: 'nav.reportables',
-                icon: null,
-                isMenuItem: true
-            },
-        },
-        {
-            path: '/new-form',
-            component: ReportPage,
-            meta: {
-                labelKey: 'nav.new-form',
                 icon: null,
                 isMenuItem: true
             },
