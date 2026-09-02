436fc84af6029c80baf2d4d3d8d42930cb434873
Lubos Babocky <Babocky.Lubos@dpb.sk>
2026-09-02 10:39:28 +0200

    update manifest.json

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,29 @@ export default defineConfig({
             workbox: {},
             navigateFallback: '/pwa-new',
             navigateFallbackDenylist: [/^\/api/],
-            manifest: {},
+            manifest: {
+                id: '/',
+                name: 'DPB - Hlásenie porúch',
+                short_name: 'Poruchy',
+                description: 'Hlásenie porúch',
+                start_url: '/',
+                scope: '/',
+                display: 'standalone',
+                theme_color: '#dc2626',
+                background_color: '#ffffff',
+                icons: [
+                    {
+                        src: '/icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/icon-512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
             injectManifest: {
                 globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
             }
