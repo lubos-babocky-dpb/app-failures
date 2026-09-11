a83e3654e63e8832cffb494fa7bcdb9f64552374
Lubos Babocky <Babocky.Lubos@dpb.sk>
2026-09-11 20:11:41 +0200

    merge seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,77 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $tables = [
+            'users',
+            'model_has_permissions',
+            'model_has_roles',
+            'role_has_permissions',
+            'permissions',
+            'roles',
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Schema::disableForeignKeyConstraints();
+
+        foreach ($tables as $table) {
+            if (Schema::hasTable($table)) {
+                DB::table($table)->truncate();
+            }
+        }
+
+        Schema::enableForeignKeyConstraints();
+
+        $user = User::create([
+            'uuid' => Str::uuid(),
+            'name' => 'Admin',
+            'email' => 'admin@dpb.sk',
+            'personal_id' => '99999',
+            'password' => Hash::make('0000'),
         ]);
+
+        $roles = [];
+
+        foreach ([
+            'super-admin',
+            'admin',
+            'driver',
+        ] as $roleName) {
+            $roles[$roleName] = Role::create([
+                'name' => $roleName,
+                'guard_name' => 'sanctuary_api',
+            ]);
+        }
+
+        $permissions = [];
+
+        foreach ([
+            'page-access.user-management',
+            'page-access.failures',
+            'page-access.failure-reports',
+            'model.user.read',
+        ] as $permissionName) {
+            $permissions[$permissionName] = Permission::create([
+                'name' => $permissionName,
+                'guard_name' => 'sanctuary_api',
+            ]);
+        }
+
+        $allPermissions = array_values($permissions);
+
+        $roles['super-admin']->syncPermissions($allPermissions);
+        $roles['admin']->syncPermissions($allPermissions);
+
+        $user->assignRole($roles['admin']);
     }
 }
