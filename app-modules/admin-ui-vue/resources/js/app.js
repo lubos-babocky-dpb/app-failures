ddd77e4d0e1b08d578e207d427aade549c48d3a4
Lubos Babocky <Babocky.Lubos@dpb.sk>
2026-09-11 20:19:45 +0200

    update app.js

--- app-modules/admin-ui-vue/resources/js/app.js
+++ app-modules/admin-ui-vue/resources/js/app.js
@@ -14,7 +14,7 @@ if ('serviceWorker' in navigator) {
         const registration = await navigator.serviceWorker.ready;
         registration.active.postMessage({type: 'sync-initial-data'});
         registration.active.postMessage({type: 'sync-user-manager-data'});
-        const apiClient = new ApiClient({baseUrl: 'http://localhost/', bearerToken: Gatekeeper.token});
+        const apiClient = new ApiClient({baseUrl: import.meta.env.APP_URL, bearerToken: Gatekeeper.token});
         const pushSubscriptionService = new PushSubscriptionService({
             vapidPublicKey: import.meta.env.VITE_VAPID_PUBLIC_KEY,
             apiClient: apiClient
