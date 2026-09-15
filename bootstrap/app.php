3911a86ab2ad399fe2c0845167b2d67610e57e85
Lubos Babocky <Babocky.Lubos@dpb.sk>
2026-09-15 23:36:57 +0200

    add api exception handing to bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,5 +18,29 @@ return Application::configure(basePath: dirname(__DIR__))
         //$middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+        $exceptions->render(function (
+            ValidationException $exception,
+            Request $request
+        ) {
+            if ($request->is('api/user-manager/*')) {
+                return response()->json([
+                    'message' => $exception->getMessage(),
+                    'errors' => $exception->errors(),
+                ], 422);
+            }
+        });
+
+        $exceptions->render(function (
+            Throwable $exception,
+            Request $request
+        ) {
+            if ($request->is('api/user-manager/*')) {
+                report($exception);
+
+                return response()->json([
+                    'message' => 'An unexpected error occurred.',
+                ], 500);
+            }
+        });
+    })
+    ->create();
